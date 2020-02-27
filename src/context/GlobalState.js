56a1734547826d177e6c4d4ab5014713bf7f8dda
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-02-27 16:45:40 +0100

    init

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,10 +14,20 @@ const initialState = {
 // Create Context
 export const GlobalContext = createContext(initialState);
 
+
+
 // Provider Component
 export const GlobalProvider = ({ children  }) => {
     const [ state, dispatch ] = useReducer(AppReducer, initialState);
 
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+    
     return (<GlobalContext.Provider value={{ transactions: state.transactions }}>
         { children }
     </GlobalContext.Provider>)
